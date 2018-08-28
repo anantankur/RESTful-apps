5ba654385e99e14c2475b3f3a9206794aae085fa
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-08-28 22:11:05 +0530

    mongodb config

--- blog-app/app.js
+++ blog-app/app.js
@@ -9,7 +9,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model('Blog', blogSchema);
 
 app.listen(3000, function(){
   console.log('restful-blog-app server has started!');
