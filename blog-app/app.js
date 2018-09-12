56c42df59d2c09319de574855d3b26212d8de0c1
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-09-12 21:58:53 +0530

    new and create route

--- blog-app/app.js
+++ blog-app/app.js
@@ -23,12 +23,30 @@ app.get('/', function(req, res){
   res.redirect('/blogs');
 });
 
+// Index Route
 app.get('/blogs', function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
       console.log(err);
     } else {
       res.render('index', {blogs: blogs});
+    }
+  });
+});
+
+// New Route
+app.get('/blogs/new', function(req, res){
+  res.render('new');
+});
+
+// Create Route
+app.post('/blogs', function(req, res){
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      console.log(err);
+      res.render('new');
+    } else {
+      res.redirect('/blogs');
     }
   });
 });
