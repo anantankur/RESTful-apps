fa12b94ae3384bea47048dae7eef807b8ff2e8b8
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-09-12 23:34:33 +0530

    delete route

--- blog-app/app.js
+++ blog-app/app.js
@@ -85,6 +85,17 @@ app.put('/blogs/:id', function(req, res){
   });
 });
 
+// Delete Route
+app.delete('/blogs/:id', function(req, res){
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs');
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log('restful-blog-app server has started!');
 });
